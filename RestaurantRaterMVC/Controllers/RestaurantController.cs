be77c3056df69667748cc14894536d8a2378eb24
lukedtitus <lukedtitus@gmail.com>
2019-04-29 11:15:49 -0400

    create view added

--- RestaurantRaterMVC/Controllers/RestaurantController.cs
+++ RestaurantRaterMVC/Controllers/RestaurantController.cs
@@ -16,5 +16,23 @@ namespace RestaurantRaterMVC.Controllers
         {
             return View(db.Restaurants.ToList());
         }
+        // GET: Restaurant/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Restaurant/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "RestaurantID,Name")] Restaurant restaurant)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Restaurants.Add(restaurant);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+        }
     }
 }
